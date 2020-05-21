71b934863d7fe17a21572bbacca9e4037afb7ac1
Sergragui <sergigrauguirado@gmail.com>
2020-05-21 14:16:23 +0200

    Segundo version del programa

--- src/circulo/Circulo.java
+++ src/circulo/Circulo.java
@@ -1,17 +1,17 @@
 package circulo;
 
-public class Circulo {
+public class Circulo implements Comparable<Circulo> {
     private double radio;
 
     public Circulo(double radio) {
         this.radio = radio;
     }
     
-    public double Perimetro(){
+    public double perimetro(){
         return 2*Math.PI*radio;
     }
     
-    public double Area(){
+    public double area(){
         return Math.PI*Math.pow(radio, 2);
     }
     
@@ -22,5 +22,20 @@ public class Circulo {
     public void setRadio(double radio) {
         this.radio = radio;
     }
+
+    @Override
+    public int compareTo(Circulo c) {
+        if(c.radio>radio){
+            return -1;
+        }else{
+            return 1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Perimetro: %.2f Area: %.2f%n", perimetro(), area());
+    }
+    
     
 }
--- src/uf5a01circulos/UF5A01Circulos.java
+++
@@ -1,9 +0,0 @@
-package uf5a01circulos;
-
-public class UF5A01Circulos {
-
-    public static void main(String[] args) {
-        
-    }
-    
-}
